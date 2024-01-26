df56839ada0a747c072cb44a1fd0608e42c342da
Vladislav Kozlovski <Valdismin@gmail.com>
2024-01-26 20:59:25 +0100

    feat: implement working threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,44 @@
+import os from 'os'
+import path from 'path'
+import {fileURLToPath} from "url";
+import {Worker} from 'worker_threads';
+
+const createPromise = (worker) => {
+    return (new Promise((resolve, reject) => {
+        worker.on('message', (message) => {
+            resolve(message.result);
+        });
+        worker.on('error', () => {
+            reject(null);
+        });
+    }));
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const numCPUs = os.cpus().length;
+    const workerPath = path.join(__dirname, 'worker.js');
+    const promises = [];
+    const workersResults = [];
+    for (let i = 0; i < numCPUs; i++) {
+        const worker = new Worker(workerPath, {
+            workerData: 10 + i
+        })
+        const promise = createPromise(worker)
+
+        promises.push(promise);
+    }
+    Promise.allSettled(promises)
+        .then((resultsArray) => {
+            resultsArray.forEach((result, index) => {
+                if (result.status === 'fulfilled') {
+                    workersResults.push({status: 'resolved', data: result.value});
+                } else {
+                    workersResults.push({status: 'error', data: null});
+                }
+            });
+            console.log(workersResults);
+        })
 };
 
 await performCalculations();
